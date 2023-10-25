c9a5597f8be95801c2cc086b6662073cf960c737
Prasad Patil <74749656+imprasadpatil@users.noreply.github.com>
2023-10-25 19:20:44 +0530

    Create implementation_of_linked_list.c

---
+++ implementation_of_linked_list.c
@@ -0,0 +1,35 @@
+// implementation of linked list  
+
+// header files 
+
+#include<stdio.h> 
+
+#include<conio.h> 
+
+#define maxsize 6 
+
+// main function  
+
+void main() 
+
+{ 
+
+// decleration  
+
+int i,a[maxsize]={27,56,7,17,36,98}; 
+
+clrscr(); 
+
+printf("\nimplimentation of linked list is as follows:\n"); 
+
+// for loop to print linked list  
+
+for(i=0;i<=maxsize-1;i++) 
+
+{ 
+
+printf("  %d",a[i]); 
+
+} 
+
+getch(); 
