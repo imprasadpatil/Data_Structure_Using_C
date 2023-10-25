1f90457085093cd6bc0f714b1bf912995c4b75e0
Prasad Patil <74749656+imprasadpatil@users.noreply.github.com>
2023-10-25 18:59:31 +0530

    Create implementation_of_queue.c

---
+++ implementation_of_queue.c
@@ -0,0 +1,37 @@
+// Implementation of queue 
+
+//header files 
+
+#include<stdio.h> 
+
+#include<conio.h> 
+
+#define maxsize 6 
+
+// main function  
+
+void main() 
+
+{ 
+
+// declaration  
+
+int i,a[maxsize]={27,56,7,17,36,98}; 
+
+clrscr(); 
+
+printf("\nimplimentation of queue is as follows:\n"); 
+
+// for loop to print queue elements  
+
+for(i=0;i<=maxsize-1;i++) 
+
+{ 
+
+printf("  %d",a[i]); 
+
+} 
+
+getch(); 
+
+} 
